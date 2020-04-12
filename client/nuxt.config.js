6d76859f86c104b0d9b0f25c0a634dd0c3539f48
Pauline Menage <pauline.menage26@gmail.com>
2020-04-12 22:34:05 +0200

    Add map and geocoder

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -14,7 +14,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' },
+      { rel: 'stylesheet', href: 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.css' }
     ]
   },
   /*
--- client/pages/trips/add.vue
+++ client/pages/trips/add.vue
@@ -1,43 +1,56 @@
 <template>
 	<v-layout align-start column>
 		<h1>Add a trip</h1>
-		<v-form ref="form" v-model="valid">
-			<v-text-field v-model="formData.name" :rules="nameRules" label="Name" required></v-text-field>
-			<v-menu
-				v-model="startMenu"
-				:close-on-content-click="false"
-				:nudge-right="40"
-				transition="scale-transition"
-				offset-y
-				min-width="290px"
-			>
-				<template v-slot:activator="{ on }">
-					<v-text-field v-model="formData.startDate" label="Start date" readonly v-on="on"></v-text-field>
-				</template>
-				<v-date-picker v-model="formData.startDate" @input="startMenu = false"></v-date-picker>
-			</v-menu>
-			<v-menu
-				v-model="endMenu"
-				:close-on-content-click="false"
-				:nudge-right="40"
-				transition="scale-transition"
-				offset-y
-				min-width="290px"
-			>
-				<template v-slot:activator="{ on }">
-					<v-text-field v-model="formData.endDate" label="End date" readonly v-on="on"></v-text-field>
-				</template>
-				<v-date-picker v-model="formData.endDate" @input="endMenu = false"></v-date-picker>
-			</v-menu>
-			<v-text-field v-model="formData.country" :rules="countryRules" label="Country" required></v-text-field>
-			<v-text-field v-model="formData.latitude" label="Latitude" required></v-text-field>
-			<v-text-field v-model="formData.longitude" label="Longitude" required></v-text-field>
-			<v-btn @click="submit">Submit</v-btn>
-		</v-form>
+		<v-container>
+			<v-row>
+				<v-col>
+					<v-form ref="form" v-model="valid">
+						<v-text-field v-model="formData.name" :rules="nameRules" label="Name" required></v-text-field>
+						<v-menu
+							v-model="startMenu"
+							:close-on-content-click="false"
+							:nudge-right="40"
+							transition="scale-transition"
+							offset-y
+							min-width="290px"
+						>
+							<template v-slot:activator="{ on }">
+								<v-text-field v-model="formData.startDate" label="Start date" readonly v-on="on"></v-text-field>
+							</template>
+							<v-date-picker v-model="formData.startDate" @input="startMenu = false"></v-date-picker>
+						</v-menu>
+						<v-menu
+							v-model="endMenu"
+							:close-on-content-click="false"
+							:nudge-right="40"
+							transition="scale-transition"
+							offset-y
+							min-width="290px"
+						>
+							<template v-slot:activator="{ on }">
+								<v-text-field v-model="formData.endDate" label="End date" readonly v-on="on"></v-text-field>
+							</template>
+							<v-date-picker v-model="formData.endDate" @input="endMenu = false"></v-date-picker>
+						</v-menu>
+						<div id="geocoder" class="geocoder"></div>
+						<v-text-field v-model="formData.country" :rules="countryRules" label="Country" required></v-text-field>
+						<v-text-field v-model="formData.latitude" label="Latitude" required></v-text-field>
+						<v-text-field v-model="formData.longitude" label="Longitude" required></v-text-field>
+						<v-btn @click="submit">Submit</v-btn>
+					</v-form>
+				</v-col>
+				<v-col>
+					<div id="map"></div>
+				</v-col>
+			</v-row>
+		</v-container>
 	</v-layout>
 </template>
 
 <script>
+import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+
 export default {
 	data: () => ({
 		valid: false,
@@ -63,6 +76,31 @@ export default {
 				this.$router.push({ path: "/trips" });
 			}
 		}
+	},
+	mounted() {
+		mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
+		const map = new mapboxgl.Map({
+			container: "map",
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [-79.4512, 43.6568],
+			zoom: 13
+		});
+
+		const geocoder = new MapboxGeocoder({
+			accessToken: mapboxgl.accessToken,
+			mapboxgl: mapboxgl,
+			placeholder: "Location"
+		});
+		document.getElementById("geocoder").appendChild(geocoder.onAdd(map));
+		geocoder.on("results", results => {
+			console.log("EVENT", results);
+		});
 	}
 };
 </script>
+
+<style lang="scss" scoped>
+#map {
+	height: 100%;
+}
+</style>
