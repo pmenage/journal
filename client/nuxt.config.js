0c9eeff7313b7d19eb2b7ec82fd4b284f1fe7d69
Pauline Menage <pauline.menage26@gmail.com>
2020-04-12 15:44:02 +0200

    Clean client

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -81,7 +81,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
