dd4850cd91ec9c63aa5cb5e5d5864300e56ba0e0
Pauline Menage <pauline.menage26@gmail.com>
2020-07-18 13:16:35 +0200

    View one trip

---
+++ client/pages/trips/_id/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-layout column>
+		<h1>{{ trip.name }}</h1>
+		<v-row>
+			<v-col>
+				<div>
+					{{ trip.country }}, from
+					{{ trip.startDate | moment('MMMM Do YYYY') }} to
+					{{ trip.endDate | moment('MMMM Do YYYY') }}
+				</div>
+			</v-col>
+		</v-row>
+	</v-layout>
+</template>
+
+<script>
+import mapboxgl from 'mapbox-gl'
+
+export default {
+	data() {
+		return {
+			trips: []
+		}
+	},
+	async asyncData({ app }) {
+		return {
+			trip: await app.$tripRepository.findAllById(1)
+		}
+	}
+}
+</script>
