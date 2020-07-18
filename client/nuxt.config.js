501e72c0b556f529468cd1a020c19a451cf8c001
Pauline Menage <pauline.menage26@gmail.com>
2020-07-18 15:22:48 +0200

    Change nuxt config

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,91 +1,95 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  mode: 'spa',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' },
-      { rel: 'stylesheet', href: 'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.css' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '~/plugins/repository',
-    '~/plugins/moment'
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxt/typescript-build',
-    '@nuxtjs/vuetify',
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv',
-  ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
-  },
-  /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
-  }
+	mode: 'spa',
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		titleTemplate: '%s - ' + process.env.npm_package_name,
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: process.env.npm_package_description || ''
+			}
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{
+				rel: 'stylesheet',
+				href: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css'
+			},
+			{
+				rel: 'stylesheet',
+				href:
+					'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.2/mapbox-gl-geocoder.css'
+			}
+		]
+	},
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: '#fff' },
+	/*
+	 ** Global CSS
+	 */
+	css: [],
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: ['~/plugins/repository', '~/plugins/moment'],
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: ['@nuxt/typescript-build', '@nuxtjs/vuetify'],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios',
+		// Doc: https://github.com/nuxt-community/dotenv-module
+		'@nuxtjs/dotenv'
+	],
+	/*
+	 ** Axios module configuration
+	 ** See https://axios.nuxtjs.org/options
+	 */
+	axios: {
+		baseURL: process.env.API_BASE_URL || 'http://localhost:8080'
+	},
+	/*
+	 ** vuetify module configuration
+	 ** https://github.com/nuxt-community/vuetify-module
+	 */
+	vuetify: {
+		customVariables: ['~/assets/variables.scss'],
+		theme: {
+			dark: false,
+			themes: {
+				dark: {
+					primary: colors.blue.darken2,
+					accent: colors.grey.darken3,
+					secondary: colors.amber.darken3,
+					info: colors.teal.lighten1,
+					warning: colors.amber.base,
+					error: colors.deepOrange.accent4,
+					success: colors.green.accent3
+				}
+			}
+		}
+	},
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {}
+	}
 }
