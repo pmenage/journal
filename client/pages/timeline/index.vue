1498435c2cbf6d91f560e765a9fde9821994d803
Pauline Menage <pauline.menage26@gmail.com>
2020-07-18 13:16:26 +0200

    Improve timeline

--- client/pages/timeline/index.vue
+++ client/pages/timeline/index.vue
@@ -2,34 +2,83 @@
 	<v-layout column>
 		<h1>My trips timeline</h1>
 		<v-row>
-			<v-list>
-				<v-list-item-group>
-					<v-list-item :key="trip.id" v-for="trip in trips">
-						<v-list-item-content>
-							<nuxt-link :to="`/trips/${trip.id}`">
-								<v-list-item-title
-									v-text="trip.name"
-								></v-list-item-title>
-							</nuxt-link>
-						</v-list-item-content>
-					</v-list-item>
-				</v-list-item-group>
-			</v-list>
+			<v-expansion-panels flat multiple>
+				<v-expansion-panel :key="trips.id" v-for="trips in tripsByYear">
+					<v-expansion-panel-header>
+						<h2>{{ trips[0] }}</h2>
+					</v-expansion-panel-header>
+					<v-expansion-panel-content>
+						<div
+							class="Trip__detail"
+							:key="trip.id"
+							v-for="trip in trips[1]"
+						>
+							<div v-text="trip.name"></div>
+							<div>
+								<v-btn icon :to="`/trips/${trip.id}`" nuxt>
+									<v-icon>mdi-eye</v-icon>
+								</v-btn>
+								<v-btn icon :to="`/trips/${trip.id}/edit`">
+									<v-icon>mdi-pencil</v-icon>
+								</v-btn>
+								<v-btn icon><v-icon>mdi-delete</v-icon></v-btn>
+							</div>
+						</div>
+					</v-expansion-panel-content>
+				</v-expansion-panel>
+			</v-expansion-panels>
 		</v-row>
 	</v-layout>
 </template>
 
-<script>
+<script lang="ts">
+import moment from 'moment'
+
+type Trip = {
+	id: number
+	name: string
+	country: string
+	startDate: string
+	endDate: string
+	latitude: number
+	longitude: number
+	createdAt: string
+	updatedAt: string
+}
+
 export default {
 	data() {
 		return {
-			trips: []
+			tripsByYearMap: new Map<number, Trip[]>()
 		}
 	},
 	async asyncData({ app }) {
+		const trips = await app.$tripRepository.findAll()
+		const tripsByYearMap = new Map<number, Trip[]>()
+		trips.forEach((trip: Trip) => {
+			const year = moment(trip.startDate).year()
+			if (tripsByYearMap.get(year)) {
+				tripsByYearMap.get(year).push(trip)
+			} else {
+				tripsByYearMap.set(year, [trip])
+			}
+		})
 		return {
-			trips: await app.$tripRepository.findAll()
+			tripsByYearMap
+		}
+	},
+	computed: {
+		tripsByYear() {
+			console.log(Array.from(this.tripsByYearMap))
+			return Array.from(this.tripsByYearMap)
 		}
 	}
 }
 </script>
+
+<style lang="scss">
+.Trip__detail {
+	display: flex;
+	justify-content: space-between;
+}
+</style>
